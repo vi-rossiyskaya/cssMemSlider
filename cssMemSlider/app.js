8bb94035591ddf59878af32736408328a1e87ace
vi-rossiyskaya <v.i.rossiyskaya@gmail.com>
2022-05-17 03:42:57 +0200

    feat: controls style interaction  Tue May 17 2022 00:42:17 GMT+0000

--- cssMemSlider/app.js
+++ cssMemSlider/app.js
@@ -5,7 +5,19 @@ const container = document.createElement('div');
 const imgWrapper = document.createElement('div');
 const controlsWrapper = document.createElement('div');
 const text = document.createElement('p')
+const buttonR = document.createElement('button')
+const buttonL = document.createElement('button')
 
+let data = [
+    {'imgUrl': 'url(./assets/img/0.jpg)',
+    'text': 'В тренажерном зале'},
+    {'imgUrl': 'url(./assets/img/1.jpg)',
+    'text': 'Мы ищем кого-то более...реалистичного'},
+    {'imgUrl': 'url(./assets/img/2.jpg)',
+    'text': 'Сейчас узнаешь!'},
+    {'imgUrl': 'url(./assets/img/3.jpg)',
+    'text': 'Как ты выдерживаешь весь этот негатив?'}
+]
 
 header.classList.add('header');
 container.classList.add('container');
@@ -13,21 +25,44 @@ main.classList.add('main');
 imgWrapper.classList.add('imgWrapper');
 controlsWrapper.classList.add('controlsWrapper');
 text.classList.add('text')
+buttonL.classList.add('buttonL')
+buttonR.classList.add('buttonR')
+
 
 for (let i = 0; i < 4; i ++) {
+    const dotBtn = document.createElement('div')
     const dot = document.createElement('div')
+    dotBtn.classList.add('dotBtn');
     dot.classList.add('dot');
-    controlsWrapper.append(dot)
+    dot.dataset.id = i;
+    dotBtn.dataset.id = i;
+    dotBtn.append(dot)
+    controlsWrapper.append(dotBtn)
 }
 
-text.append('Sample text')
+text.append(`${data[0].text}`)
 header.append('Wawa Wiwa Comics')
 body.append(header)
 body.append(main)
 main.append(container)
+imgWrapper.append(buttonR)
+imgWrapper.append(buttonL)
 container.append(imgWrapper)
 container.append(text)
 container.append(controlsWrapper)
+
+controlsWrapper.addEventListener('click', (e) => {
+    console.log(e)
+    console.log(e.target)
+    console.log(e.target.dataset.id)
+})
+controlsWrapper.addEventListener('mouseover', (e) => {
+    if (e.target.classList.contains('active')) {return}
+    controlsWrapper.querySelector('.dot[data-id=' + `\'${e.target.dataset.id}\']`).classList.add('dotHover')
+})
+controlsWrapper.addEventListener('mouseout', (e) => {
+    controlsWrapper.querySelector('.dot[data-id=' + `\'${e.target.dataset.id}\']`).classList.remove('dotHover')
+})
 
 
 
